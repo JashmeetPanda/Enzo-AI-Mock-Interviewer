5fb7f074b81ed8bf85e39a31e0e302138f5daab6
Jashmeet <160411804+JashmeetPanda@users.noreply.github.com>
2025-05-07 02:41:01 +0530

    Redirection to dashboard fixed

--- app/page.js
+++ app/page.js
@@ -1,10 +1,17 @@
+'use client'
+
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
+  const handleRedirect = () => {
+    window.location.href = "https://enzo-sand.vercel.app/dashboard";
+  };
+
   return (
     <div>
       <h1>Start</h1>
-      <Button>Click</Button>
-    </div>  );
+      <Button onClick={handleRedirect}>Click</Button>
+    </div>
+  );
 }
